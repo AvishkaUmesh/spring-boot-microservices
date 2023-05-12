0c2a1587eddf82fc02ca6a1736819bbd9547c03e
Avishka Umesh <umesh.avishka@gmail.com>
2023-05-12 11:57:51 +0530

    service-registry added (eureka)

--- employee-service/src/main/java/com/avishka/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/avishka/employeeservice/EmployeeServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableFeignClients
 public class EmployeeServiceApplication {
-    
+
     public static void main(String[] args) {
         SpringApplication.run(EmployeeServiceApplication.class, args);
     }
