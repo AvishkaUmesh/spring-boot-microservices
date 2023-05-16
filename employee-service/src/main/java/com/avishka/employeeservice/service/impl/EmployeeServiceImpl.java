dedc6eaead8b5844b05991d1fb2285eb0ee40828
Avishka Umesh <umesh.avishka@gmail.com>
2023-05-16 15:13:52 +0530

    added CircuitBreaker pattern

--- employee-service/src/main/java/com/avishka/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/com/avishka/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -8,10 +8,13 @@ import com.avishka.employeeservice.exception.EmailAlreadyExistException;
 import com.avishka.employeeservice.exception.ResourceNotFoundException;
 import com.avishka.employeeservice.mapper.EmployeeMapper;
 import com.avishka.employeeservice.repository.EmployeeRepository;
-import com.avishka.employeeservice.service.APIClient;
 import com.avishka.employeeservice.service.EmployeeService;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Optional;
 
@@ -19,8 +22,11 @@ import java.util.Optional;
 @AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeServiceImpl.class);
+
     private EmployeeRepository employeeRepository;
-    private APIClient apiClient;
+    //    private APIClient apiClient;
+    private WebClient webClient;
 
     @Override
     public EmployeeDTO save(EmployeeDTO employeeDTO) {
@@ -38,14 +44,23 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     }
 
+    //    @CircuitBreaker(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
+    @Retry(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
     @Override
     public APIResponseDTO getEmployeeById(Long id) {
 
+        LOGGER.info("Inside getEmployeeById() method of EmployeeServiceImpl");
         Employee employee = employeeRepository.findById(id).orElseThrow(
                 () -> new ResourceNotFoundException("Employee", "id", id)
         );
 
-        DepartmentDTO departmentDTO = apiClient.getDepartmentByCode(employee.getDepartmentCode());
+//        DepartmentDTO departmentDTO = apiClient.getDepartmentByCode(employee.getDepartmentCode());
+
+        DepartmentDTO departmentDTO = webClient.get()
+                .uri("http://localhost:8080/api/departments/" + employee.getDepartmentCode())
+                .retrieve()
+                .bodyToMono(DepartmentDTO.class)
+                .block();
 
         APIResponseDTO apiResponseDTO = new APIResponseDTO();
         apiResponseDTO.setDepartment(departmentDTO);
@@ -53,4 +68,23 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         return apiResponseDTO;
     }
+
+    public APIResponseDTO getDefaultDepartment(Long id, Exception ex) {
+        LOGGER.info("Inside getDefaultDepartment() method of EmployeeServiceImpl");
+        Employee employee = employeeRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Employee", "id", id)
+        );
+
+        DepartmentDTO departmentDTO = new DepartmentDTO();
+        departmentDTO.setDepartmentCode("0000");
+        departmentDTO.setDepartmentName("Default Department");
+        departmentDTO.setDepartmentDescription("Default Department Description");
+
+        APIResponseDTO apiResponseDTO = new APIResponseDTO();
+        apiResponseDTO.setDepartment(departmentDTO);
+        apiResponseDTO.setEmployee(EmployeeMapper.MAPPER.employeeToEmployeeDTO(employee));
+
+        return apiResponseDTO;
+    }
+
 }
